12fe5fa5f8c7806be4ab62647c56b3b7fe95df7c
LuciMcD <lucimcdaniel@gmail.com>
2023-09-08 20:55:41 -0500

    cleaning up the log and fixing mistakes

--- user_numeric_lists.py
+++ user_numeric_lists.py
@@ -20,9 +20,9 @@ logger, logname = setup_logger(__file__)
 # TODO: define some custom functions
 #list_1 is a list of numbers representing the number of times a foster child moved homes. The data is made up. 
 list_1 = [18, 7, 20, 5, 10, 7, 8, 2, 6, 12, 8, 9, 3, 11, 5, 6, 15, 4, 5, 7]
-listx = [range(1,10)] #ages of foster children
+listx = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] #ages of foster children
 listy = [6320, 10201, 10504, 11200, 9386, 5754, 5202, 5300, 4800, 4000]#yearly costs for child care based on age
-
+lst = [10, 20, 30, 40]
 
 def list1_statistics():
     logger.info(f"list_1: {list_1}")
@@ -38,24 +38,29 @@ def list1_statistics():
     stdev = statistics.stdev(list_1)
     variance = statistics.variance(list_1)
 
-    logger.info(f"stdev: {stdev}")
-    logger.info(f"variance: {variance}")
+    logger.info(f"stdev: {stdev: .2f}")
+    logger.info(f"variance: {variance: .2f}")
+    logger.info(f"")
+    logger.info(f"")
+
 
 def list_correlation_prediction():
     logger.info(f"listx: {listx}")
     logger.info(f"listy: {listy}")
 
-    correlationxy = statistics.correlation(listx, listy)
-    logger.info(f"correlation between x and y: {correlationxy}")
+    correlationx_y = statistics.correlation(listx, listy)
+    logger.info(f"correlation between x and y: {correlationx_y: .2f}")
 
     slope, intercept = statistics.linear_regression(listx, listy)
-    logger.info(f"The equation of the best fit line is: y = {slope}x + {intercept}")
+    logger.info(f"The equation of the best fit line is: y = {slope: .2f}x + {intercept: .2f}")
 
     futurex = 15 #this futurex value is the value given in the project instructions
 
     newy = slope * futurex + intercept
     
-    logger.info(f"We predict that when x = {futurex}, y will be {newy}")
+    logger.info(f"We predict that when x = {futurex}, y will be {newy: .2f}")
+    logger.info(f"")
+    logger.info(f"")
 
 def list_1_built_in_functions():
     min_value = min(list_1)
@@ -80,48 +85,59 @@ def list_1_built_in_functions():
     desc1 = sorted(list_1, reverse=True)
     logger.info(f"Using the sorted reverse function we get: {desc1}")
 
+    logger.info(f"")
+    logger.info(f"")
+
 def list_methods():
-    lst = [10, 20, 30, 40]
+    logger.info(f"lst: {lst}")
 
     lst.append(50)
+    logger.info(f"The appended list is: {lst}")
 
     lst.extend([5, 10, 15, 20])
+    logger.info(f"The extended lst is: {lst}")
 
     lst.insert(2, 25)
+    logger.info(f"lst after inserting values: {lst}")
 
     lst.remove(10)
+    logger.info(f"The lst after removing a value: {lst}")
 
     count_of_20 = lst.count(20)
     logger.info(f"the number of 20's is {count_of_20}")
 
-    asclst = lst.sort()
-    logger.info(f"The ascending list is: {asclst}")
+    lst.sort()
+    logger.info(f"The ascending list is: {lst}")
 
-    desclst = lst.sort(reverse=True)
-    logger.info(f"The descending list is: {desclst}")
+    lst.sort(reverse=True)
+    logger.info(f"The descending list is: {lst}")
 
-    lstcopy = lst.copy
-    logger.info(f"The lstcopy is {lstcopy}")
+    lst.copy
+    logger.info(f"The lstcopy is {lst}")
 
-    first = lstcopy.pop(0)
-    logger.info(f"Popped the first item off the top of lstcopy ({first}).Now lstcopy is {lstcopy}")
+    first = lst.pop(0)
+    logger.info(f"Popped the first item off the top of lstcopy ({first}). Now lstcopy is {lst}")
 
-    last = lstcopy.pop(-1)
-    logger.info(f"Popped the last item off the end of lstcopy ({last}). Now lstcopy is {lstcopy}")
-
+    last = lst.pop(-1)
+    logger.info(f"Popped the last item off the end of lstcopy ({last}). Now lstcopy is {lst}")
+    logger.info(f"")
+    logger.info(f"")
 
 def list_1_transformations():
     logger.info(f"List_1= {list_1}")
 
-    under10list = [filter(lambda x: x < 10, list_1)]
-    logger.info(f"Times moved under 10: {under10list}")
+    under10 = list(filter(lambda x: 10 > x, list_1))
+    logger.info(f"Times moved under 10: {under10}")
 
 
-    cubedroot = map(lambda x: math.cbrt(x), list_1)
-    logger.info(f"The cubed root of the numbers is: {cubedroot}")
+    cubedroot = list(map(lambda x: math.cbrt(x), list_1))
+    roundcbrt = [round(x,2) for x in cubedroot ]
+    logger.info(f"The cubed root of the numbers is: {roundcbrt}")
 
-    volume = map(lambda x: x**3, list_1)
+    volume = list(map(lambda x: x**3, list_1))
     logger.info(f"The volume of a cube with x length is: {volume}")
+    logger.info(f"")
+    logger.info(f"")
 
 def list_1_comprehension():
     logger.info(f"List_1: {list_1}")
@@ -132,9 +148,9 @@ def list_1_comprehension():
     triplelist = [ x*3 for x in list_1]
     logger.info(f"The list-1 tripled is: {triplelist}")
 
-    evenslist = [ x for x in list_1 if x / 2 == 0]
+    evenslist = [ x for x in list_1 if x % 2 == 0]
     logger.info(f"The even numbers in list_1 are: {evenslist}")
-
+    logger.info(f"")
 
 def show_log():
     """Read log file and print it to the terminal"""
